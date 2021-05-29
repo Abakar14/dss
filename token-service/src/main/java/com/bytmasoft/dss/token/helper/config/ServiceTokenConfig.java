5d368e6774c512cb87583602c27748ec6ab51fed
Abakar14 <abakar61@web.de>
2021-05-29 13:52:09 +0200

    added anew bean in ServiceTokenConfig

---
+++ token-service/src/main/java/com/bytmasoft/dss/token/helper/config/ServiceTokenConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.bytmasoft.dss.token.helper.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Mahamat Abakar created on 26.05.2021 at 12:46:23
+ */
+@Configuration
+public class ServiceTokenConfig {
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+}
